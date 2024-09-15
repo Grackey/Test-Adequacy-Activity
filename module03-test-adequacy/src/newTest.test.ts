045142bdb9d4478f5d01750c777a904f1c5309f1
bramjohnson <bbcola18@gmail.com>
2024-09-15 16:25:15 -0400

    Kill mutants related to addGrade

--- module03-test-adequacy/src/newTest.test.ts
+++ module03-test-adequacy/src/newTest.test.ts
@@ -1,15 +1,30 @@
 import * as db from "./transcriptManager";
 
+// This test needs to come first, or else db.init will override
+describe('Runtime validation', () => {
+    it('Should start with an empty list of transcripts', () => {
+        expect(db.getAll()).toEqual([]);
+    })
+})
+
 describe('Within database', () => {
     beforeEach(() => {
         // Before any test runs, clean up the datastore. This should ensure that tests are hermetic.
         db.initialize();
-    })    
+    })
 
     describe("Adding grade", () => {
         it('Should ensure that grades initialize as empty list if not given', () => {
             const studentID = db.addStudent('test student');
             expect(db.getTranscript(studentID)?.grades).toEqual([]);
+        });
+
+        it('Should throw an error if there is no student with the given ID', () => {
+            const fakeStudentID = 103;
+            const course = 'example course'
+            expect(() => {
+                db.addGrade(fakeStudentID, course, 88)
+            }).toThrowError(`no student with ID = ${fakeStudentID}`);
         });
 
         it('Should throw an error if the student already has a grade in that course', () => {
@@ -18,7 +33,7 @@ describe('Within database', () => {
             db.addGrade(studentID, 'duplicate course', 100)
             expect(() => {
                 db.addGrade(studentID, course, 88)
-            }).toThrowError();
+            }).toThrowError(`student ${studentID} already has a grade in course ${course}`);
         });
     })
 
@@ -31,22 +46,22 @@ describe('Within database', () => {
             }).toThrowError()
         })
     })
-})
+});
 
 describe('demo students are correcly added to db', () => {
     beforeAll(() => {
         // Before all of the tests run, clean up the datastore and add demo data.
         db.initialize();
         db.addDemoData();
-    }) 
+    })
 
     it('Should successfully add avery to the database when adding demo data', () => {
         const averyID = db.getStudentIDs('avery');
         expect(averyID).not.toBeNull;
         expect(averyID.length).toEqual(1);
         const averyTranscript = db.getTranscript(averyID[0]);
-        expect(averyTranscript?.grades).toEqual([{"course": "DemoClass", "grade": 100}, {"course": "DemoClass2", "grade": 100}]);
-        expect(averyTranscript?.student.studentName).toEqual('avery');  
+        expect(averyTranscript?.grades).toEqual([{ "course": "DemoClass", "grade": 100 }, { "course": "DemoClass2", "grade": 100 }]);
+        expect(averyTranscript?.student.studentName).toEqual('avery');
     })
 
     it('Should successfully add the first blake to the database when adding demo data', () => {
@@ -54,8 +69,8 @@ describe('demo students are correcly added to db', () => {
         expect(blakeID).not.toBeNull;
         expect(blakeID.length).toEqual(2);
         const blakeTranscript = db.getTranscript(blakeID[0]);
-        expect(blakeTranscript?.grades).toEqual([{"course": "DemoClass", "grade": 80}]);
-        expect(blakeTranscript?.student.studentName).toEqual('blake');  
+        expect(blakeTranscript?.grades).toEqual([{ "course": "DemoClass", "grade": 80 }]);
+        expect(blakeTranscript?.student.studentName).toEqual('blake');
     })
 
 
@@ -64,8 +79,8 @@ describe('demo students are correcly added to db', () => {
         expect(blakeID).not.toBeNull;
         expect(blakeID.length).toEqual(2);
         const blakeTranscript = db.getTranscript(blakeID[1]);
-        expect(blakeTranscript?.grades).toEqual([{"course": "DemoClass", "grade": 85}, {"course": "DemoClass", "grade": 40}]);
-        expect(blakeTranscript?.student.studentName).toEqual('blake');  
+        expect(blakeTranscript?.grades).toEqual([{ "course": "DemoClass", "grade": 85 }, { "course": "DemoClass", "grade": 40 }]);
+        expect(blakeTranscript?.student.studentName).toEqual('blake');
     })
 
     it('Should successfully add casey to the database when adding demo data', () => {
@@ -73,7 +88,7 @@ describe('demo students are correcly added to db', () => {
         expect(caseyID).not.toBeNull;
         expect(caseyID.length).toEqual(1);
         const caseyTranscript = db.getTranscript(caseyID[0]);
-        expect(caseyTranscript?.grades).toEqual([{"course": "DemoClass", "grade": 100}]);
-        expect(caseyTranscript?.student.studentName).toEqual('casey');  
+        expect(caseyTranscript?.grades).toEqual([{ "course": "DemoClass", "grade": 100 }]);
+        expect(caseyTranscript?.student.studentName).toEqual('casey');
     })
 })
